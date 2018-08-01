e2e9dcb60a01340ad04b735bb266e1158cf3c88d
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-08-01 09:21:33 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,9 +3,6 @@ require_relative './class_methods_module.rb'
 
 
 class Kid
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
-
   attr_accessor :name
 
   def initialize(name)
