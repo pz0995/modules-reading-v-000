c8cca8d8d7b54c76070a3a643aafc4515c4d6abd
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-08-01 09:18:40 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,7 +1,11 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
+
 class Dancer
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
   include Dance
   extend MetaDancing
 
